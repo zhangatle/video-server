746e130661e840c56c85eed15a48d1e948942aff
zhangatle <319126583@qq.com>
2020-05-21 20:24:33 +0800

    test

---
+++ api/defs/apidef.go
@@ -0,0 +1,34 @@
+package defs
+
+// requests
+type UserCredential struct {
+	Username string `json:"username"`
+	Pwd string `json:"pwd"`
+}
+
+// response
+type SignedUp struct {
+	Success bool `json:"success"`
+	SessionId string `json:"session_id"`
+}
+
+// Data model
+type VideoInfo struct {
+	Id string
+	AuthorId int
+	Name string
+	DisplayCtime string
+}
+
+type Comment struct {
+	Id string
+	VideoId string
+	Author string
+	Content string
+}
+
+type SimpleSession struct {
+	Username string
+	TTL int64
+}
+
