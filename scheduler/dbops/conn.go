ee74bc7299b48f4ae62164ef2047d88b4c7dd1d4
zhangatle <319126583@qq.com>
2020-05-22 14:22:51 +0800

    完成调度器部分

--- scheduler/dbops/conn.go
+++ scheduler/dbops/conn.go
@@ -1,1 +1,18 @@
 package dbops
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	dbConn *sql.DB
+	err error
+)
+
+func init() {
+	dbConn, err = sql.Open("mysql","root:Itzler.666@tcp(192.168.10.99:3306)/video_server?charset=utf8mb4")
+	if err != nil {
+		panic(err.Error())
+	}
+}
--- scheduler/main.go
+++ scheduler/main.go
@@ -1,1 +1,18 @@
-package scheduler
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+	router.GET("/video-delete-record/:vid-id", vidDelREcHandler)
+	return router
+}
+
+func main()  {
+	go taskrunner.Start()
+	r := RegisterHandlers()
+	http.ListenAndServe(":9001", r)
+}
